5f970c937c911b6c357377905873b4037ac10fa5
Eric de Guzman <eric_deguzman@bmc.com>
2024-06-07 14:10:38 -0700

    Updates

--- get-articles.sh
+++
@@ -1,2 +0,0 @@
-curl -X GET \
-  'https://firestore.googleapis.com/v1/projects/auxilium-420904/databases/aux-db/documents/articles' -v
--- new-article.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST \
-  'https://firestore.googleapis.com/v1/projects/auxilium-420904/databases/aux-db/documents/articles' \
-  -H 'Content-Type: application/json' \
-  -d '{
-        "fields": {
-          "title": { "stringValue": "Funny guy" },
-          "body": { "stringValue": "Lorem funny" }
-        }
-      }'
